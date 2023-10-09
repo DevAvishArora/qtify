c5bb6ff8f3cbde97c820f9aa61ed5b3726527420
avish.arora <avish.arora@walkweltech.com>
2023-10-09 14:36:09 +0530

    navbar-qtify

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import QtifyLogo from '../../assets/qtify.png'
+const Logo = () => {
+  return (
+    <img src={QtifyLogo} alt='' width={67} />
+  )
+}
+
+export default Logo
