6268b605066b354961f5017c0b4d849f4d908d56
avish.arora <avish.arora@walkweltech.com>
2023-10-19 16:24:18 +0530

    card-qtify-takehome

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Hero from "./components/Hero/Hero";
+import Navbar from "./components/Navbar/Navbar";
+import { fetchTopAlbums } from "./api/api";
+import Section from "./components/Section/Section";
 
 function App() {
+  const [topAlbumsData, setAlbumsData] = useState([]);
+
+  const generateTopAlbumsData = async () => {
+    try {
+      const data = await fetchTopAlbums();
+      setAlbumsData(data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    generateTopAlbumsData();
+  }, []);
+
   return (
-    <div >
+    <div>
       <Navbar />
+      <Hero />
+      <div>
+        <Section data={topAlbumsData} type="album" title="Top Albums" />
+      </div>
     </div>
   );
 }
---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from "react";
+import styles from "./Carousel.module.css";
+import 'swiper/css';
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import CarouselLeftNavigation from "./CarouselLeftNavigation";
+import CarousalRightNavigation from "./CarousalRightNavigation";
+
+const Controls = ({ data }) => {
+
+  const swiper = useSwiper();
+
+  useEffect(() => {
+    swiper.slideTo(0,null);
+  }, [data]);
+
+  return <></>;
+};
+const Carousel = ({ data, renderComponent }) => {
+  return (
+    <div className={styles.wrapper}>
+      <Swiper
+        style={{ padding: "0px 20px" }}
+        initialSlide={0}
+        modules={[Navigation]}
+        slidesPerView={"auto"}
+        spaceBetween={40}
+        allowTouchMove
+      >
+        <Controls data={data} />
+        <CarouselLeftNavigation />
+        <CarousalRightNavigation />
+        {
+          data.map(item => (
+            <SwiperSlide>{renderComponent(item)}</SwiperSlide>
+          ))
+        }
+      </Swiper>
+    </div>
+  );
+};
+
+export default Carousel;
